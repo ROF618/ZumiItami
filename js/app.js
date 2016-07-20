f538a07a8b8639f397f475c0042d22a8952a1ff9
Chris Bravo <crbravo618@gmail.com>
2016-07-19 20:36:46 -0500

    Centered thumbnails and added intro CSS animations

--- js/app.js
+++ js/app.js
@@ -17,4 +17,31 @@ $(document).ready(function() {
 			}
 		}
 	});
+	var $animation_elementsU = $(".slideUp");
+	var $window = $(window);
+
+	$window.on('scroll', check_if_in_view);
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+
+	  $.each($animation_elementsU, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+
+	     if ((element_bottom_position >= window_top_position) &&
+	        (element_top_position <= window_bottom_position)) {
+	      		$element.addClass('animated bounceInUp');
+
+	    } else {
+	      $element.addClass('animate');
+	    }
+	  });
+	}
 });
